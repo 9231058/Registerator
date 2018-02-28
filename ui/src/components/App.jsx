501ff4768a5079d7e0a5a1ad9a82b03346e36539
Parham Alvani <parham.alvani@gmail.com>
2018-03-01 02:29:10 +0330

    First page and http client is ready.

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import axios from 'axios'
 
 // We are using bootstrap as the UI library
 import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends React.Component {
+  constructor () {
+    super()
+
+    this.client = axios.create({
+      baseURL: `${window.location.href}api`
+    })
+
+    this.state = {
+      about: 'loading'
+    }
+  }
+
+  componentDidMount () {
+    this.client.get('/about').then((response) => {
+      this.setState({
+        about: response.data
+      })
+    })
+  }
+
   render () {
     return (
-      <h1>Welcome to CEIT Registerator</h1>
+      <div>
+        <h1>Welcome to CEIT Registerator</h1>
+        <h2>{this.state.about}</h2>
+      </div>
     )
   }
 }
