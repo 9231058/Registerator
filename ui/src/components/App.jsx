6084a24bd0ee0de1687d28c70d1730599ac9346e
Parham Alvani <parham.alvani@gmail.com>
2018-03-01 09:29:49 +0330

    Corrects formating issues.

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -26,65 +26,64 @@ class App extends React.Component {
   }
 
   handleSubmit (event) {
-    event.preventDefault();
+    event.preventDefault()
   }
 
   render () {
     return (
-      <div className="container">
-        <div className="py-5 text-center">
+      <div className='container'>
+        <div className='py-5 text-center'>
           <h1>Welcome to CEIT Registerator</h1>
           <h2>{this.state.about}</h2>
-          <p className="lead">
+          <p className='lead'>
             Please fill the following form with your information
             currectly.
           </p>
         </div>
-        <div className="row">
-          <div className="col-8">
-            <form className="needs-validation" onSubmit={this.handleSubmit}>
-              <div className="row">
-                <div className="col-6 mb-3">
-                  <label htmlFor="firstName">First name</label>
-                  <input type="text" className="form-control" id="firstName" placeholder="" required></input>
-                  <div className="invalid-feedback" style={{width: "100%"}}>
+        <div className='row'>
+          <div className='col-8'>
+            <form className='needs-validation' onSubmit={this.handleSubmit}>
+              <div className='row'>
+                <div className='col-6 mb-3'>
+                  <label htmlFor='firstName'>First name</label>
+                  <input type='text' className='form-control' id='firstName' placeholder='' required />
+                  <div className='invalid-feedback' style={{width: '100%'}}>
                     Your First name is required.
                   </div>
                 </div>
-                <div className="col-6 mb-3">
-                  <label htmlFor="lastName">Last name</label>
-                  <input type="text" className="form-control" id="lastName" placeholder="" required></input>
-                  <div className="invalid-feedback" style={{width: "100%"}}>
+                <div className='col-6 mb-3'>
+                  <label htmlFor='lastName'>Last name</label>
+                  <input type='text' className='form-control' id='lastName' placeholder='' required />
+                  <div className='invalid-feedback' style={{width: '100%'}}>
                     Your Last name is required.
                   </div>
                 </div>
               </div>
-              <div className="mb-3">
-                <label htmlFor="studentID">Username</label>
-                <input type="text" className="form-control" id="stduentID" placeholder="9231058" required></input>
-                <div className="invalid-feedback" style={{width: "100%"}}>
+              <div className='mb-3'>
+                <label htmlFor='studentID'>Username</label>
+                <input type='text' className='form-control' id='stduentID' placeholder='9231058' required />
+                <div className='invalid-feedback' style={{width: '100%'}}>
                   Your StudentID is required.
                 </div>
               </div>
-              <div className="mb-3">
-                <label htmlFor="email">Email</label>
-                <input type="email" className="form-control" id="email" placeholder="you@example.com" required></input>
-                <div className="invalid-feedback" style={{width: "100%"}}>
+              <div className='mb-3'>
+                <label htmlFor='email'>Email</label>
+                <input type='email' className='form-control' id='email' placeholder='you@example.com' required />
+                <div className='invalid-feedback' style={{width: '100%'}}>
                   Your Email is required.
                 </div>
               </div>
             </form>
-            <hr className="mb-4" />
-            <button className="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
+            <hr className='mb-4' />
+            <button className='btn btn-primary btn-lg btn-block' type='submit'>Submit</button>
           </div>
-          <div className="col-4">
-          </div>
+          <div className='col-4' />
         </div>
-        <footer className="my-5 pt-5 text-muted text-center text-small">
-          <p className="mb-1">&copy; 2018 AUT-CEIT</p>
-          <ul className="list-inline">
-            <li className="list-inline-item"><a href="https://github.com/AUT-CEIT/Registerator">Github</a></li>
-            <li className="list-inline-item"><a href="mailto:parham.alvani@gmail.com">Support</a></li>
+        <footer className='my-5 pt-5 text-muted text-center text-small'>
+          <p className='mb-1'>&copy; 2018 AUT-CEIT</p>
+          <ul className='list-inline'>
+            <li className='list-inline-item'><a href='https://github.com/AUT-CEIT/Registerator'>Github</a></li>
+            <li className='list-inline-item'><a href='mailto:parham.alvani@gmail.com'>Support</a></li>
           </ul>
         </footer>
       </div>
